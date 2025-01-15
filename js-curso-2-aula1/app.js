dee70a18ca044e1864763e2df7b82e01cd5d238b
renancvitor <renan.vitor.cm@gmail.com>
2025-01-15 16:42:51 -0300

    Fim do módulo 02

--- js-curso-2-aula1/app.js
+++ js-curso-2-aula1/app.js
@@ -1,9 +1,17 @@
-let titulo = document.querySelector('h1');
-titulo.innerHTML = `Jogo do número secreto.`;
+let numeroSecreto = gerarNumeroAleatorio();
 
-let paragrafo = document.querySelector('p');
-paragrafo.innerHTML = `Escolha um número entre 1 e 10.`;
+function exibeTextoTela(tag, texto) {
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
+}
+
+exibeTextoTela('h1', `Jogo do número secreto.`);
+exibeTextoTela('p', `Escolha um número entre 1 e 10.`);
 
 function verificarpalpite() {
-    
+    let palpite = document.querySelector('input').value;
+}
+
+function gerarNumeroAleatorio() {
+    return parseInt(Math.random() * 10 + 1);
 }
